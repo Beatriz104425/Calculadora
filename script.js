da68062367e5b8e9d94df8a164971f32bb17d1f0
Beatriz104425 <beatriz.porto.o@uni9.edu.br>
2026-05-07 23:09:15 +0000

    ....

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ function insertToDisplay(data){
 function clean(){
     document.querySelector('#display').value =''
 }
+
+function back(){
+    const display = document.querySelector('#display')
+    display.value = display.value.slice(0, -1)
+}
+
+function result(){
+    const display = document.querySelector('#display')
+    try{
+        display.value = eval(display.value)
+    } catch{
+
+    }
+}
